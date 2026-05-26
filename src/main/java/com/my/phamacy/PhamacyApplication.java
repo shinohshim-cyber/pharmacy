53c052656465005032356f6118a290c1acb6b3d4
sos-cyber <shinohshim@gmail.com>
2026-05-26 12:32:26 +0900

    feat : first commit

---
+++ src/main/java/com/my/phamacy/PhamacyApplication.java
@@ -0,0 +1,13 @@
+package com.my.phamacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamacyApplication.class, args);
+	}
+
+}
